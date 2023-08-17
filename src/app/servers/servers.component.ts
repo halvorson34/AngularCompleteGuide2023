3d38e962ca87d23aa700ecefc73031a7b1ccfbc2
[Matt Halvorson] <[matt.halvorson@hrblock.com]>
2023-08-17 13:38:25 -0500

    add string interpolation for getting server status and id in server component

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  //selector: 'app-servers', // selects by element
+  selector: 'app-servers', // selects by element
   //selector: '[app-servers]', // selects by attribute
-  selector: '.app-servers', // selects by attribute
+  //selector: '.app-servers', // selects by attribute
   template: '<app-server></app-server><app-server></app-server>', //inline template instead of templateUrl
   styleUrls: ['./servers.component.css']
 })
